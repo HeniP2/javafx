f0956784bce46d5a884521a8c39191dd884e9ae6
Your Name <you@example.com>
2024-09-26 13:36:55 +0200

    initial commit2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,6 +15,7 @@ public class App extends Application {
 
     private static Scene scene;
 
+    //modify
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
